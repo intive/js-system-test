cdc517e3ffcd738913ce2d7b09d93778e800692b
Antoni Mrowiński <58977190+AntoniMrowinski@users.noreply.github.com>
2020-05-05 17:58:52 +0200

    Offline Snackbar Test (#1)
    * Offline Snackbar Test
    
    * fix test for 96, add test for 98
    
    * Offline Snackbar Test
    
    * fix test for 96, add test for 98
    
    * 96_98

---
+++ src/test/java/selenium/NoConnectionTest.java
@@ -0,0 +1,57 @@
+package selenium;
+
+import org.openqa.selenium.WebElement;
+import org.testng.Assert;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+import selenium.base.TestBase;
+import selenium.pages.NoConnectionPage;
+import java.io.IOException;
+
+public class NoConnectionTest extends TestBase {
+    private NoConnectionPage noConnectionPage;
+
+    @BeforeClass
+    public void startPage() {
+        noConnectionPage = new NoConnectionPage(driver);
+
+    }
+    @Test
+    public void testNoInternetMessage() throws IOException {
+        noConnectionPage.goTo();
+        noConnectionPage.internetConnection(false);
+        WebElement theRightSnackBar;
+        WebElement theIncorrectSnackBar;
+        WebElement theRightExitButton;
+
+        try {
+            Assert.assertEquals(noConnectionPage.snackBar.getText(), "Hej, coś nie styka! Sprawdź połączenie.");
+            theRightSnackBar = noConnectionPage.snackBar;
+            theRightExitButton = noConnectionPage.exitButton;
+            theIncorrectSnackBar = noConnectionPage.snackBar2;
+        } catch (AssertionError e) {
+            try {
+                Assert.assertEquals(noConnectionPage.snackBar2.getText(), "Hej, coś nie styka! Sprawdź połączenie.");
+                theRightSnackBar = noConnectionPage.snackBar2;
+                theRightExitButton = noConnectionPage.exitButton2;
+                theIncorrectSnackBar = noConnectionPage.snackBar;
+            }
+            catch (AssertionError e2){
+                throw e2;
+            }
+        }
+        noConnectionPage.snackBarExist(theIncorrectSnackBar,10);
+
+        boolean snackBarExistenceConfirmation = noConnectionPage.snackBarExist(theRightSnackBar, 10);
+        Assert.assertEquals(snackBarExistenceConfirmation, true, "Nie wykryto prawidłowego snackbaru.");
+        noConnectionPage.clickElement(theRightExitButton);
+        snackBarExistenceConfirmation = noConnectionPage.snackBarDoesntExist(theRightSnackBar, 10);
+        Assert.assertEquals(snackBarExistenceConfirmation, false, "Snackbar nie zniknął.");
+        snackBarExistenceConfirmation = noConnectionPage.snackBarExist(theRightSnackBar, 10);
+        Assert.assertEquals(snackBarExistenceConfirmation, true, "Snackbar nie pojawił się ponownie.");
+
+        noConnectionPage.internetConnection(true);
+        snackBarExistenceConfirmation = noConnectionPage.snackBarDoesntExist(theRightSnackBar, 10);
+        Assert.assertEquals(snackBarExistenceConfirmation, false, "Snackbar nie zniknął po wznowieniu połączenia internetowego.");
+    }
+}
