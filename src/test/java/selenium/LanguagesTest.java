f80b3ed1718d56433069967248c021a37931e596
AntoniMrowinski <antoni.mrowinski@gmail.com>
2020-06-02 14:22:33 +0200

    languages partial fix 2 + modifications

--- src/test/java/selenium/LanguagesTest.java
+++ src/test/java/selenium/LanguagesTest.java
@@ -1,12 +1,11 @@
 package selenium;
 
+import org.testng.asserts.SoftAssert;
 import selenium.base.TestBase;
-import org.testng.Assert;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 import selenium.pages.LanguageDashboardPage;
 import java.io.IOException;
-import java.util.concurrent.TimeUnit;
 
 public class LanguagesTest extends TestBase {
     private LanguageDashboardPage languagePage;
@@ -14,71 +13,72 @@ public class LanguagesTest extends TestBase {
     @BeforeClass
     public void startPage() {
         languagePage = new LanguageDashboardPage(driver);
+        languagePage.removeAllSensorsFromMap();
         languagePage.goTo();
-        driver.manage().timeouts().implicitlyWait(10000, TimeUnit.MILLISECONDS);
     }
 
     @Test(priority = 0)
     public void languageLabelsAndSnackbarsTest () throws IOException, InterruptedException {
-        languagePage.mapLoaded(languagePage.homePlan, 5);
-        languagePage.waitingForLanguageLabelVisibility();
-//          Sprawdzanie poprawności tłumaczenia labelek i komunikatów, także przy zmianie języka, gdy nie ma połączenai internetowego
-        languagePage.verifyPlTranslation();
+        SoftAssert total_Assertion = new SoftAssert();
+        languagePage.mapExists();
+        languagePage.languageLabelExists();
+        languagePage.verifyPlTranslation(total_Assertion);
         languagePage.internetConnection(false);
-        languagePage.verifySnackbarsPl();
-        languagePage.clickElement(languagePage.languageLabel);
-        languagePage.clickElement(languagePage.enLanguageLabel);
-        languagePage.verifyEnTranslation();
-        languagePage.verifySnackbarsEn();
-        languagePage.clickElement(languagePage.languageLabel);
-        languagePage.clickElement(languagePage.plLanguageLabel);
-        languagePage.verifySnackbarsPlAfterLanguageChange();
+        languagePage.verifySnackbarsPl(total_Assertion);
+        languagePage.switchLanguage("en");
+        languagePage.verifyEnTranslation(total_Assertion);
+        languagePage.verifySnackbarsEn(total_Assertion);
+        languagePage.switchLanguage("pl");
+        languagePage.verifySnackbarsPlAfterLanguageChange(total_Assertion);
+        languagePage.internetConnection(true);
         languagePage.clickElement(languagePage.sensor1);
         languagePage.clickToAddPoint(-50, -75);
-//          Tu chcę zaimplementować kod, który będzie sprawdzać treść snacka o nieudanym umiejscowieniu czujnika.
-//          Obecnie tego nie robię, gdyż taki kod powinien być bardziej generyczny.
-        languagePage.internetConnection(true);
-        Thread.sleep(1000);
+        languagePage.clickElement(languagePage.sensor2);
+        languagePage.clickToAddPoint(-70, -95);
+        languagePage.verifyProximityWarningPl();
+        languagePage.clickElement(languagePage.sensorProximityWarningExitButton);
+        languagePage.switchLanguage("en");
+        languagePage.clickElement(languagePage.sensor3);
+        languagePage.clickToAddPoint(90, 100);
+        languagePage.clickElement(languagePage.sensor4);
+        languagePage.clickToAddPoint(110, 120);
+        languagePage.verifyProximityWarningEn();
         languagePage.goTo();
+        languagePage.languageLabelExists();
         languagePage.internetConnection(false);
-//         Sprawdzenie komunikatu o nieudanym załadowaniu mapy Home oraz tłumaczenia przycisku odświeżania
-//         Zmieniany jest też język, aby sprawdzić te 2 napisy w wersji angielskiej
-        String loadMapSnackBar = languagePage.loadMapSnackBar.getText();
-        String refreshButton = languagePage.refreshButton.getText();
-        languagePage.verifyLoadMapPl(loadMapSnackBar, refreshButton);
-        languagePage.clickElement(languagePage.languageLabel);
-        languagePage.clickElement(languagePage.enLanguageLabel);
-        loadMapSnackBar = languagePage.loadMapSnackBar.getText();
-        refreshButton = languagePage.refreshButton.getText();
-        languagePage.verifyLoadMapEn(loadMapSnackBar, refreshButton);
-//        powrot do stanu poczatkowego
+        languagePage.switchLanguage("pl");
+        String loadMapSnackBar = languagePage.getElementTranslations(languagePage.loadMapSnackBar);
+        String refreshButton = languagePage.getElementTranslations(languagePage.refreshButton);
+        languagePage.verifyLoadMapPl(total_Assertion, loadMapSnackBar, refreshButton);
+        languagePage.switchLanguage("en");
+        loadMapSnackBar = languagePage.getElementTranslations(languagePage.loadMapSnackBar);
+        refreshButton = languagePage.getElementTranslations(languagePage.refreshButton);
+        languagePage.verifyLoadMapEn(total_Assertion, loadMapSnackBar, refreshButton);
         languagePage.internetConnection(true);
-        languagePage.clickElement(languagePage.languageLabel);
-        languagePage.clickElement(languagePage.plLanguageLabel);
+        languagePage.switchLanguage("pl");
+        total_Assertion.assertAll();
     }
 
     @Test(priority = 1)
     public void languageChoiceMemory () throws IOException, InterruptedException {
-//        Test sprawdza, czy strona zapamiętuje wybór użytkownika w kontekście wybranego wcześniej języka.
+        SoftAssert total_Assertion = new SoftAssert();
         languagePage.goTo();
-        languagePage.waitingForLanguageLabelVisibility();
-        languagePage.mapLoaded(languagePage.homePlan, 5);
-        Thread.sleep(1000);
-        Assert.assertEquals(languagePage.getElementTranslations(languagePage.languageLabel), "PL", "Przeglądarka nie jest odpalona w języku polskim.");
-        languagePage.verifyPlTranslation();
-        languagePage.clickElement(languagePage.languageLabel);
-        languagePage.clickElement(languagePage.enLanguageLabel);
-        languagePage.verifyEnTranslation();
+        languagePage.languageLabelExists();
+        languagePage.mapExists();
+//        Thread.sleep(1000);
+        languagePage.verifyChosenLanguagePl();
+        languagePage.verifyPlTranslation(total_Assertion);
+        languagePage.switchLanguage("en");
+        languagePage.verifyEnTranslation(total_Assertion);
         languagePage.internetConnection(false);
-        languagePage.verifySnackbarsEn();
+        languagePage.verifySnackbarsEn(total_Assertion);
         languagePage.internetConnection(true);
         languagePage.goTo();
-        languagePage.waitingForLanguageLabelVisibility();
-        Assert.assertEquals(languagePage.getElementTranslations(languagePage.languageLabel), "EN", "Nie zapamiętano uprzednio wybranego języka");
-        languagePage.verifyEnTranslation();
-//          powrot do stanu poczatkowego
+        languagePage.languageLabelExists();
+        languagePage.verifyChosenLanguageEn();
+        languagePage.verifyEnTranslation(total_Assertion);
         languagePage.internetConnection(true);
-        languagePage.clickElement(languagePage.languageLabel);
-        languagePage.clickElement(languagePage.plLanguageLabel);
+        languagePage.switchLanguage("pl");
+        total_Assertion.assertAll();
     }
 }
